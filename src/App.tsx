1fee288e58cf0deba192b2d889a451bc77dca6a8
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-05-04 17:16:08 +0000

    created pages and implemented react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import './App.css'
-import Timer from './projects/project-01/timer';
-import TodoList from './projects/project-02/todoList';
-import TicTacToe from './projects/project-03/game';
-import MultiSelectInput from './projects/project-04/multiselect';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-    <div>
-      <section id="project-01">
-        <p className="project__title">1. Timer App</p>
-        <Timer initialTime={120} />
-      </section>
-      <section id="project-02">
-        <p className="project__title">2. Todo App</p>
-        <TodoList />
-      </section>
-      <section id="project-03">
-        <p className="project__title">3. Tic-Tac-Toe</p>
-        <TicTacToe />
-      </section>
-      <section id="project-04">
-        <p className="project__title">4. Multiselect Input</p>
-        {/* <MultiSelectInput onSelectionChange={selectedOptions} options={inputValue}/> */}
-      </section>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects/>} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,29 @@
+import Timer from '../projects/project-01/timer';
+import TodoList from '../projects/project-02/todoList';
+import TicTacToe from '../projects/project-03/game';
+import MultiSelectInput from '../projects/project-04/multiselect';
+
+const Projects = () => {
+  return (
+        <div>
+      <section id="project-01">
+        <p className="project__title">1. Timer App</p>
+        <Timer initialTime={120} />
+      </section>
+      <section id="project-02">
+        <p className="project__title">2. Todo App</p>
+        <TodoList />
+      </section>
+      <section id="project-03">
+        <p className="project__title">3. Tic-Tac-Toe</p>
+        <TicTacToe />
+      </section>
+      <section id="project-04">
+        <p className="project__title">4. Multiselect Input</p>
+        {/* <MultiSelectInput onSelectionChange={selectedOptions} options={inputValue}/> */}
+      </section>
+    </div>
+  );
+};
+
+export default Projects;
